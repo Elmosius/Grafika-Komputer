64613cf180f6882fc764646fbc8fea9e755f23aa
Elmosius <2272008@maranatha.ac.id>
2024-12-04 22:59:00 +0700

    checkpoint

--- pertemuan_11_lighting/prak/nomor_1/index.js
+++ pertemuan_11_lighting/prak/nomor_1/index.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 import * as dat from "dat.gui";
-
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
-import PlaneMesh from "./plane_mesh.js";
 
+// SCENE 1
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 const cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const controls = new OrbitControls(cam, renderer.domElement);
@@ -20,25 +20,29 @@ document.body.appendChild(renderer.domElement);
 cam.position.z = 5;
 cam.position.y = 2;
 
-let pLight = new THREE.PointLight(0xffffff, 2);
-pLight.position.set(-1, 1, 2);
+// material bolanya
+const metal_texture = new THREE.TextureLoader().load("../textures/metal/Metal049A_2K-JPG_Color.jpg");
+const metal_normal = new THREE.TextureLoader().load("../textures/metal/Metal049A_2K-JPG_NormalGL.jpg");
+const metalness = new THREE.TextureLoader().load("../textures/metal/Metal049A_2K-JPG_Metalness.jpg");
+const metal_rough = new THREE.TextureLoader().load("../textures/metal/Metal049A_2K-JPG_Roughness.jpg");
+
+// bola
+let sphereGeo = new THREE.SphereGeometry(0.5, 16, 16);
+let sphereMat = new THREE.MeshStandardMaterial({ map: metal_texture, normalMap: metal_normal, roughness: metal_rough, metalness: metalness });
+let sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+sphereMesh.position.set(0, 0.5, 0);
+scene.add(sphereMesh);
+
+// lampu
+let ambient = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambient);
+
+let pLight = new THREE.PointLight(0xffffff, 10);
+pLight.position.set(0, 1.5, -0.5);
 scene.add(pLight);
-scene.add(new THREE.PointLightHelper(pLight, 0.1, 0xff0000));
+scene.add(new THREE.PointLightHelper(pLight, 0.3, 0xff0000));
 
-// box 1
-let cGeo = new THREE.BoxGeometry(1, 1, 1);
-let cMat = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-let cMesh = new THREE.Mesh(cGeo, cMat);
-cMesh.position.set(1, 0.5, 0);
-scene.add(cMesh);
-
-// box 2
-let cGeo2 = new THREE.BoxGeometry(1, 1, 1);
-let cMat2 = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-let cMesh2 = new THREE.Mesh(cGeo2, cMat2);
-cMesh2.matrixAutoUpdate = false;
-cMesh2.position.set(-2, 0.5, 0);
-scene.add(cMesh2);
+const plane = new PlaneMesh(scene);
 
 // controll gui
 let kendali = new Object();
@@ -51,9 +55,6 @@ gui.add(kendali, "x", -4, 4, -0.1);
 gui.add(kendali, "y", -4, 4);
 gui.add(kendali, "z", -4, 4);
 
-const plane = new PlaneMesh(scene);
-// plane.position.y -= 0.5;
-
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   cam.aspect = window.innerWidth / window.innerHeight;
@@ -63,16 +64,16 @@ window.addEventListener("resize", () => {
 let angle = Math.PI / 4;
 function draw() {
   controls.update();
-  cMesh.rotation.x += 0.01;
+  //   cMesh.rotation.x += 0.01;
   angle += 0.01;
   //   cMesh.scale.x += 0.01;
   //   mesh.rotation.y += 0.01;
-  let rMatrix = new THREE.Matrix4().makeRotationX(angle);
-  let tMatrix = new THREE.Matrix4().makeTranslation(0, 1, 0);
-  let result = new THREE.Matrix4().multiplyMatrices(rMatrix, tMatrix);
-  cMesh2.matrix.fromArray(result.toArray());
+  //   let rMatrix = new THREE.Matrix4().makeRotationX(angle);
+  //   let tMatrix = new THREE.Matrix4().makeTranslation(0, 1, 0);
+  //   let result = new THREE.Matrix4().multiplyMatrices(rMatrix, tMatrix);
+  //   cMesh2.matrix.fromArray(result.toArray());
 
-  pLight.position.set(kendali.x, kendali.y, kendali.z);
+  ambient.position.set(kendali.x, kendali.y, kendali.z);
   renderer.render(scene, cam);
   requestAnimationFrame(draw);
 }
